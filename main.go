2100c326dade41c4af5679a383a2085faa9e7ffb
Logan Cole <emtlogancole@gmail.com>
2025-08-26 12:19:05 -0500

    add basic models

--- main.go
+++ main.go
@@ -1,6 +1,28 @@
 package main
 
+import (
+	"log"
+	"net/http"
+
+	"github.com/exasperlnc/go-vanillajs/logger"
+)
+func initializeLogger() *logger.Logger {
+	logInstance, err := logger.NewLogger("movie.log")
+	if err != nil {
+		log.Fatalf("Could not initialize logger: %v", err)
+	}
+	defer logInstance.Close()
+	return logInstance
+}
 
 func main() {
-
+	logInstance := initializeLogger()	
+	
+	http.Handle("/", http.FileServer(http.Dir("public")))
+	const addr = ":8080"
+	err := http.ListenAndServe(addr, nil)
+	if err != nil {
+		log.Fatalf("Server Failed: %v", err)
+		logInstance.Error("Server Failed", err)
+	}
 }
---
+++ models/movie.go
@@ -0,0 +1,17 @@
+package models
+
+type Movie struct {
+	ID          int
+	TMDB_ID     int
+	Title       string
+	ReleaseYear int
+	Tagline     string
+	Overview    *string
+	Score       *float32
+	Popularity  *float32
+	Keywords    []string
+	Language    *string
+	PosterURL   *string
+	TrailerURL  *string
+	Casting     []Actor
+}
